73ba763b1c7bfad6f1def0e39b33c9b80b13b370
RacyFiend <perchemidi.da@phystech.edu>
2025-02-22 23:48:46 +0300

    Initial Commit

---
+++ SocialNetworkWithPhotosTests/SocialNetworkWithPhotosTests.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkWithPhotosTests.swift
+//  SocialNetworkWithPhotosTests
+//
+//  Created by Дмитрий Перчемиди on 22.02.2025.
+//
+
+import Testing
+@testable import SocialNetworkWithPhotos
+
+struct SocialNetworkWithPhotosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
