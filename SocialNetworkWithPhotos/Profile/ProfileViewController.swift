7d4f54177d4cedf739f9e028dca5994a2fbd0b11
RacyFiend <demonperechemidi@gmail.com>
2025-03-07 11:32:25 +0300

    Добавлен экран с профайлом

---
+++ SocialNetworkWithPhotos/Profile/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  SocialNetworkWithPhotos
+//
+//  Created by Дмитрий Перчемиди on 05.03.2025.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+}
