2296e3ea5b29ed452d25796b5cdc40eec7ac6885
RacyFiend <demonperechemidi@gmail.com>
2025-05-10 00:54:56 +0300

    Добавлено перечисление с константами

---
+++ SocialNetworkWithPhotos/Constants.swift
@@ -0,0 +1,10 @@
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "dUm5n0cs9aKacoTDc47Lgt4-jwtw22s0hF1xDc9mG8Y"
+    static let secretKey = "2IbOTsV1jHVWiv0Mp8XBDHjDMmLexmC6JfkVdnpaZX0"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
